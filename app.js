bab81221af4fe16c106d61efbe0bb4d31c28c35e
root <root@ip-172-31-28-157.eu-west-1.compute.internal>
2019-02-21 13:06:32 +0000

    final tuning

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello shuja abedin');
+  res.send('hello shuja');
 });
  
 app.listen(process.env.PORT || 5000);
